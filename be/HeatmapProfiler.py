a2a32a016b636d4a282ab11b7cd0716e4119c129
Jessica Yang <jessicayzt@hotmail.com>
2019-11-09 15:16:01 -0800

    Start HeatmapProfiler

---
+++ be/HeatmapProfiler.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import pprofile
+
+# Take profile
+# Manipulate to json to pass off to Jeff/Greg
+
+
+class HeatmapProfiler:
+    def __init__(self):
+        self._profiler = pprofile.Profile() # p.profile object, which will import
+        # interact with profile, suck data out of it, dump to json in structure
+        #json.dump to pump out json
+
+
+
+
+
+
