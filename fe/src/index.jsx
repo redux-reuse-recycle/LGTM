8de11db4abdd7888c9bd91aac6ccad0d9ad3ee0a
Jeffrey Doyle <jeffreydoylec@gmail.com>
2019-11-21 11:56:29 -0800

    Changed router to HashRouter so we can load the proj locally without a server

--- fe/src/index.jsx
+++ fe/src/index.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import path from 'path';
 import {
-    BrowserRouter as Router,
+    HashRouter as Router,
 } from "react-router-dom";
 import dotenv from 'dotenv';
 
